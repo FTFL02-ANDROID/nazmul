c25079053aec3af4ec3f0fcc46a36d5b999f1a6b
mdNazmulHasan <najmul.hasan10@gmail.com>
2015-01-29 14:36:26 +0600

    complete
    at last everything is working fine

---
+++ PublicUniversity_v2/src/com/nazmul/publcuniversity_v2/util/PublicUniversityConstants.java
@@ -0,0 +1,10 @@
+package com.nazmul.publcuniversity_v2.util;
+
+public class PublicUniversityConstants  {
+	
+	public static final String SELECTED_ID="selected_id";
+	public static final String LATITUDE="latitude";
+	public static final String LONGITUDE="longitude";
+	public static final String RESTAURANTNAME="RestaurantaName";
+
+}
